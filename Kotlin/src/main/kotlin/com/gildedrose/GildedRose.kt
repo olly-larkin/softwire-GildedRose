6571fee406cd120e9065d92d689482eb4d9d2f00
Olly Larkin <olly.larkin@softwire.com>
2020-07-07 12:30:58 +0100

    partial refactoring

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,51 +3,63 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+        for (item in items) {
+//            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+//                if (item.quality > 0) {
+//                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+//                        item.quality = item.quality - 1
+//                    }
+//                }
+//            } else {
+//                if (item.quality < 50) {
+//                    item.quality = item.quality + 1
+//
+//                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+//                        if (item.sellIn < 11) {
+//                            if (item.quality < 50) {
+//                                item.quality = item.quality + 1
+//                            }
+//                        }
+//
+//                        if (item.sellIn < 6) {
+//                            if (item.quality < 50) {
+//                                item.quality = item.quality + 1
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+            when (item.name) {
+                "Aged Brie" -> if (item.quality < 50) item.quality += 1
+                "Backstage passes to a TAFKAL80ETC concert" ->
+                    when (item.sellIn) {
+                        in Int.MIN_VALUE..0 -> item.quality = 0
+                        in 1..5 -> if (item.quality <= 47) item.quality += 3 else item.quality = 50
+                        in 6..10 -> if (item.quality <= 48) item.quality += 2 else item.quality = 50
+                        else -> if (item.quality < 50) item.quality += 1
                     }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+                "Sulfuras, Hand of Ragnaros" -> {}
+                else -> if (item.quality > 0) item.quality -= 1
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > 0) {
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
