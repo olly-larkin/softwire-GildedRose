7073eaf93e64d08f24ae7ea8ee58c30f2ce01f08
Abel Shields <abel@uucp.org.uk>
2020-07-07 13:45:32 +0100

    Added legacy test

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,5 +1,6 @@
 package com.gildedrose
 
+import com.sun.corba.se.impl.resolver.BootstrapResolverImpl
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -129,6 +130,38 @@ class GildedRoseTest {
                 app.items[1].quality == startQuality + 3)
         assertTrue("Quality of $backstage_pass_name should drop to 0 after concert",
                 app.items[2].quality == 0)
+    }
+
+    @Test
+    fun test_same_as_legacy() {
+        // Want to test that backstage, brie, and other items `foo` all behave the same
+        // NOT TESTING "conjured" -- Doesn't exist in legacy code!!
+
+        val numItems = 10
+        val numItersEach = 10
+        val numItersTotal = 100
+        val itemNames = arrayOf(backstage_pass_name, aged_brie_name, sulfuras_name, "foo")
+
+        for (iter in 1..numItersTotal) {
+            val items = mutableListOf<Item>()
+            for (i in 1..numItems) {
+                items.add(
+                        Item(itemNames.random(), (1..numItersEach).random(), (1..49).random())
+                )
+            }
+            val thisApp = GildedRose(items.toTypedArray())
+            val referenceApp = GildedRoseLegacy(items.toTypedArray())
+
+            for (i in 1..numItersEach) {
+                thisApp.updateQuality()
+                referenceApp.updateQuality()
+                thisApp.items.zip(referenceApp.items) {
+                    thisItem, refItem -> assertTrue("$thisItem, $refItem should be equal",
+                        thisItem.quality == refItem.quality &&
+                        thisItem.sellIn == refItem.sellIn)
+                }
+            }
+        }
     }
 
 }
